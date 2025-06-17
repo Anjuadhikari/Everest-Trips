9ed00c662a335d1028ac88d2661fccfa9d54614e
Anju Adhikari <anjuadhkari667@gmail.com>
2025-06-17 22:03:26 +0545

    navbar changes

--- component/top-navbar.tsx
+++ component/top-navbar.tsx
@@ -10,23 +10,23 @@ import React from "react";
 export default function TopNavbar() {
   return (
     <div className="bg-blue-600 text-white p-4 flex justify-between items-center ">
-     <div className="flex items-center space-x-8">
-       <div className="flex items-center space-x-2">
-        <Mail />
-        <p>example@gmail.com</p>
+      <div className="flex items-center space-x-8">
+        <div className="flex items-center space-x-2">
+          <Mail />
+          <p>example@gmail.com</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Phone />
+          <p>+977 1234567890</p>
+        </div>
       </div>
-      <div className="flex items-center space-x-2"  >
-        <Phone />
-        <p>+977 1234567890</p>
-      </div>
-
-     </div>
-      <div className="flex items-center space-x-4 ">
-        <Instagram className="hover:opacity-50" />
-        <Facebook className="hover:opacity-50" />
-        <Twitter className="hover:opacity-50" />
-        <Linkedin size={23} className="hover:opacity-50" />
+      <div className="flex items-center space-x-4">
+        <Instagram className="hover:opacity-50 cursor-pointer" />
+        <Facebook className="hover:opacity-50 cursor-pointer" />
+        <Twitter className="hover:opacity-50 cursor-pointer" />
+        <Linkedin size={23} className="hover:opacity-50 cursor-pointer" />
       </div>
     </div>
+   
   );
 }
